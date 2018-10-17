9abfca1af79a5c9a72f96e6c21d459e437ebe39f
yiwen song <yiws@yelp.com>
2018-10-16 23:11:36 -0700

    remove python2 from supported python versions

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+"""setup.py
+
+setup.py for cbpro2 python package.
+"""
 from setuptools import find_packages
 from setuptools import setup
 
-install_requires = [
+INSTALL_REQUIRES = [
     'pymongo>=3.5.1',
     'python-dateutil',
     'requests>=2.13.0',
@@ -11,29 +15,49 @@ install_requires = [
     'websocket-client>=0.40.0',
 ]
 
-tests_require = [
+TESTS_REQUIRE = [
     'pytest',
     'coverage',
 ]
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setup(
     name='cbpro2',
-    version='1.0.0',
+    version='1.0.1',
     author='Daniel Paquin',
     author_email='dpaq34@gmail.com',
     license='MIT',
     url='https://github.com/yiwensong/coinbasepro-python',
     packages=find_packages(),
-    install_requires=install_requires,
-    tests_require=tests_require,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
     description='The unofficial Python client for the Coinbase Pro API',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    keywords=['gdax', 'gdax-api', 'orderbook', 'trade', 'bitcoin', 'ethereum', 'BTC', 'ETH', 'client', 'api', 'wrapper',
-              'exchange', 'crypto', 'currency', 'trading', 'trading-api', 'coinbase', 'pro', 'prime', 'coinbasepro'],
+    keywords=[
+        'gdax',
+        'gdax-api',
+        'orderbook',
+        'trade',
+        'bitcoin',
+        'ethereum',
+        'BTC',
+        'ETH',
+        'client',
+        'api',
+        'wrapper',
+        'exchange',
+        'crypto',
+        'currency',
+        'trading',
+        'trading-api',
+        'coinbase',
+        'pro',
+        'prime',
+        'coinbasepro',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -41,12 +65,7 @@ setup(
         'Intended Audience :: Information Technology',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
